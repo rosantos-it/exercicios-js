363f4967ab89da41e246c97c7e0b6f0f64229088
rosantos-it <rogerios.j.r@hotmail.com>
2023-05-02 19:05:56 -0300

    Ajuste nos heads de todas as páginas html e ajuste do exercícios de calculadora.

--- Aula20/scripts/pratica2.js
+++ Aula20/scripts/pratica2.js
@@ -1,41 +1,3 @@
-//--Exemplo 1 Não Funciona =/ --//
-
-function soma() {
-    let resp = document.getElementById("resp");
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let texto = '';
-
-    if (document.getElementById('soma').press)
-        texto = soma(num1, num2);
-    if (document.getElementById('subtracao').press)
-        texto = subtracao(num1, num2);
-    if (document.getElementById('multiplicacao').press)
-        texto = multiplicacao(num1, num2);
-    if (document.getElementById('divisao').press)
-        texto = divisao(num1, num2);
-
-    resp.innerHTML = texto;
-}
-
-function soma(x, y) {
-    return (x + y);
-}
-
-function subtracao(x, y) {
-    return (x - y);
-}
-
-function multiplicacao(x, y) {
-    return (x * y);
-}
-
-function divisao(x, y) {
-    return (x / y);
-}
-
-//--Exemplo 2 Não Funciona =/ --//
-
 function main()
 {
  var resp = document.getElementById('resp');
@@ -78,7 +40,3 @@ function divisao(x, y)
   
   return (x/y);
 }
-
-
-
-
